0c93d0c77082e30020aa1da17b5f31f3675ee950
Fiqri Code <fiqriawan36@gmail.com>
2025-11-10 18:10:11 +0800

    first commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Admin Dashboard')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title">Welcome to Admin Dashboard</h5>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [App\Http\Controllers\AdminController::class, 'index'])->name('dashboard');
+});
